65654fa8469e235d3eefe0a3f370f679b56f3b2d
Pamavoc <58072737+Pamavoc@users.noreply.github.com>
2024-11-23 13:49:24 +0100

    planetarium fix

--- prisma/migrations/20241122121907_dev/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Planet" DROP CONSTRAINT "Planet_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "Stats" ADD CONSTRAINT "Stats_planetId_fkey" FOREIGN KEY ("planetId") REFERENCES "Planet"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
