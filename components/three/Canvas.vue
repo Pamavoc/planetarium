3bfd6486b984a0cc4f1944ec1a5b151dab55ec36
Pamavoc <58072737+Pamavoc@users.noreply.github.com>
2024-11-23 13:08:34 +0100

    planetarium fix

--- components/three/Canvas.vue
+++ components/three/Canvas.vue
@@ -1,10 +1,11 @@
 <template>
-  <TresCanvas class="canvas">
+  <TresCanvas>
     <TresPerspectiveCamera
       :position="[3, 3, 3]"
       :look-at="[0, 0, 0]"
     />
     <ThreePlanet :apparence="apparence" />
+    <TresOrbitControls />
     <TresAmbientLight :intensity="1" />
   </TresCanvas>
 </template>
@@ -14,14 +15,4 @@ import { TresCanvas } from '@tresjs/core'
 const apparence = reactive({
   color: '#00ffff',
 })
-
-console.log(apparence)
-
 </script>
-
-<style>
-.canvas {
-  height: 50vh;
-  width: 100%;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="canvas-container">
     <ThreeCanvas />
   </div>
 </template>
@@ -9,6 +9,12 @@
 // const planets = fetchPlanets()
 </script>
 
-<style>
-
+<style lang="scss">
+.canvas-container {
+  width: 100%;
+  height: 50vh;
+  position: relative;
+  overflow: hidden;
+  background-color: #000;
+}
 </style>
