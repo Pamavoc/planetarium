aa3494d46b583e671807fe28efb9fe8b6c8205e2
Pamavoc <58072737+Pamavoc@users.noreply.github.com>
2024-11-24 11:05:10 +0100

    deploy

--- components/three/Canvas.vue
+++ components/three/Canvas.vue
@@ -5,12 +5,14 @@
       :look-at="[0, 0, 0]"
     />
     <ThreePlanet :apparence="apparence" />
-    <TresOrbitControls />
+    <OrbitControls />
     <TresAmbientLight :intensity="1" />
   </TresCanvas>
 </template>
 <script setup lang='ts'>
-import { TresCanvas } from '@tresjs/core'
+import { TresCanvas, extend  } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+
 
 const apparence = reactive({
   color: '#00ffff',
